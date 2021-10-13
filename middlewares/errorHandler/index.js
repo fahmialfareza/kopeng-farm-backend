e70469667986a1c31b3581a355ae6e8c02e2d8c3
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-10-13 08:36:22 +0700

    Fixing create user, sign in, and get profile

--- middlewares/errorHandler/index.js
+++ middlewares/errorHandler/index.js
@@ -1,4 +1,12 @@
 module.exports = (err, req, res, next) => {
+  if (err.code === 11000) {
+    err.messages = [];
+
+    for (const property in err.keyValue) {
+      err.messages.push(`${property} has been exist`);
+    }
+  }
+
   res.status(err.statusCode || 500).json({
     errors: err.messages || [err.message],
   });
